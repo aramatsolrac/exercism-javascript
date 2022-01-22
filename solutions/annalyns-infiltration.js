178e19e9a6e266e03bcad43c1e1e484dbaf5141b
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-01-21 22:03:01 -0700

    Update annalyns-infiltration.js

--- solutions/annalyns-infiltration.js
+++ solutions/annalyns-infiltration.js
@@ -4,15 +4,15 @@ const prisonerIsAwake = true;
 const petDogIsPresent = false;
 
 function canExecuteFastAttack(knightIsAwake) {
-    return !knightIsAwake
+    return !knightIsAwake;
 };
 
 function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
-    return knightIsAwake || archerIsAwake || prisonerIsAwake
+    return knightIsAwake || archerIsAwake || prisonerIsAwake;
 }
 
 function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
-    return !archerIsAwake && prisonerIsAwake
+    return !archerIsAwake && prisonerIsAwake;
 }
 
 function canFreePrisoner(
@@ -21,7 +21,7 @@ function canFreePrisoner(
     prisonerIsAwake,
     petDogIsPresent
 ) {
-    return (!archerIsAwake && petDogIsPresent) || (!knightIsAwake && !archerIsAwake && prisonerIsAwake && !petDogIsPresent)
+    return (!archerIsAwake && petDogIsPresent) || (!knightIsAwake && !archerIsAwake && prisonerIsAwake && !petDogIsPresent);
 }
 
 console.log(canFreePrisoner(false, true, false, false)); // => false
