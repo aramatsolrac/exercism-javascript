aa9463c7e1f251662f9fe8993e6432adbce3c453
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-04-13 17:39:04 -0600

    Remove breaks

--- solutions/mixedJuices.js
+++ solutions/mixedJuices.js
@@ -2,16 +2,13 @@ function timeToMixJuice(name) {
   switch (name) {
     case "Pure Strawberry Joy":
       return 0.5;
-      break;
 
     case "Energizer":
     case "Green Garden":
       return 1.5;
-      break;
 
     case "Tropical Island":
       return 3;
-      break;
 
     case "All or Nothing":
       return 5;
